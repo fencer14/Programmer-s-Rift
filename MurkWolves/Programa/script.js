e8c16d54db07b28f4f558f8e2ced24ce427eb2fa
fancer1447 <pepejorge1447@gmail.com>
2023-10-24 13:59:55 -0300

    Murk Wolves
    -Programa realizado
    -Reorganización de carpetas

---
+++ MurkWolves/Programa/script.js
@@ -0,0 +1,6 @@
+let nombre = prompt("Nombre");
+let edad = prompt("edad");
+let año = 2023 - edad;
+
+console.log("Hola "+nombre+", naciste en el año "+ año);
+window.alert("Hola "+nombre+", naciste en el año "+ año);
